9dbe40ec5787dc09cc807638ab0323b57860437a
MaxVinogradov <vinogradov.max49@gmail.com>
2018-06-24 22:33:36 +0300

    Lambda that fetch and parse AWS availability from RSS and push notification to Telegram chat.

---
+++ src/functions/awsToTgNotifier.js
@@ -0,0 +1,20 @@
+const { fetchAndParseRss } = require('../processors/rssProcessors');
+const { telegraphProcessor } = require('../processors/telegraphProcessors');
+const { sendMessage } = require('../processors/telegramProcessors');
+const awsRssToTgMapper = require('../mappers/awsRssToTgMapper')
+
+const awsToTgNotifier = async(event, context, callback) => {
+  const rssData = await fetchAndParseRss('http://status.aws.amazon.com/rss/all.rss');
+  const pageData = await telegraphProcessor('AWS Service Status', awsRssToTgMapper(rssData));
+  console.log('Created ph page: ', pageData);
+  const tgSenderResult = await sendMessage(`
+    ${rssData.title}
+    Issues count:  ${rssData.items.length}
+    Details: ${pageData.url}
+  `);
+  console.log('Tg Sender Result', tgSenderResult);
+};
+
+module.exports = {
+  awsToTgNotifier,
+};
